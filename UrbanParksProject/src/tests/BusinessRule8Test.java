3f6757ec8fce2cdbb5b0c8b557ad579cbaa7d5c5
Mike Overby <MikeOverby@outlook.com>
2015-05-28 14:34:26 -0700

    Testing the BR8, not receivejob.

--- UrbanParksProject/src/tests/BusinessRule8Test.java
+++ UrbanParksProject/src/tests/BusinessRule8Test.java
@@ -1,19 +1,19 @@
 package tests;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Before;
-import org.junit.Test;
-
-import model.DataPollster;
 import model.Job;
 import model.JobList;
 import model.ParkManager;
-import model.Schedule;
 import model.UserList;
+import model.businessRules.BusinessRule8;
+
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * This class tests the eighth business rule.
@@ -26,36 +26,29 @@ public class BusinessRule8Test {
 	UserList myUserList;
 	JobList myJobList;
 	
+    BusinessRule8 br8;
+    
+    List<String> parkList;
+    
 	@Before
 	public void setUp() throws Exception {
-		myUserList = new UserList();
-		myJobList = new JobList();
-		Schedule.getInstance().setJobList(myJobList);
-		Schedule.getInstance().setUserList(myUserList);
-		DataPollster.getInstance().setJobList(myJobList);
-		DataPollster.getInstance().setUserList(myUserList);
+		br8 = new BusinessRule8();
 		
-		
-		List<String> parkList = new ArrayList<String>();
+		parkList = new ArrayList<String>();
 		parkList.add("Namek");
 		parkList.add("Konoha");
 		parkList.add("Kento");
 		parkList.add("Egypt");
-		Tenenberg = new ParkManager("ten@uw.edu", "Mr", "Teacher", parkList);
-		myUserList.addNewUser(Tenenberg);
-		
-
 	}
-	
 	
 	/**
 	 * This tests whether a job can be created with a good park.
 	 */
 	@Test
-	public void useParkTest() {
+	public void testParkForGoodPark() {
 		Job findSasuke = new Job(1, "Konoha", 2, 2, 0, "06032015", "06032015", "ten@uw.edu", new ArrayList<ArrayList<String>>());
 		
-		assertEquals(Schedule.getInstance().receiveJob(findSasuke), true);
+		assertTrue(br8.test(findSasuke, parkList));
 	}
 	
 	
@@ -63,16 +56,10 @@ public class BusinessRule8Test {
 	 * This tests whether a job can be created with a bad park.
 	 */
 	@Test
-	public void useParkTest2() {
+	public void testParkForBadPark() {
 		Job findSasuke = new Job(1, "Seattle Park", 2, 2, 0, "06032015", "06032015", "ten@uw.edu", new ArrayList<ArrayList<String>>());
-		
-		String ex = "The park for this job is not in your"
-				+ "list of managed parks.";
-		try {
-			Schedule.getInstance().receiveJob(findSasuke);
-		} catch (Exception e) {
-			assertEquals(ex, e.getMessage());
-		}
+
+		assertFalse(br8.test(findSasuke, parkList));
 	}
 	
 }
