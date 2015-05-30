798d4d62d5dc615a1b1b4f5f54e02e4e63910f91
Taylor <tjsg1992@gmail.com>
2015-05-30 03:30:31 -0700

    Implement AllTests, a Test Suite.
    DataPollsterTest is currently commented out.

---
+++ UrbanParksProject/src/tests/AllTests.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AdministratorTest.class, BusinessRule1Test.class,
+		BusinessRule2Test.class, BusinessRule3Test.class,
+		BusinessRule4Test.class, BusinessRule5Test.class,
+		BusinessRule6Test.class, BusinessRule7Test.class,
+		BusinessRule8Test.class, /*DataPollsterTest.class, */JobTest.class,
+		ParkManagerTest.class, SaveLoadTest.class, ScheduleTest.class,
+		VolunteerTest.class })
+public class AllTests {
+
+}
