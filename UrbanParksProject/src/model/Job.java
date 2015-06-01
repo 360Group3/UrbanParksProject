83546ea6a0f1375a894d7fdb9118791ed59fb85f
Taylor <tjsg1992@gmail.com>
2015-05-31 19:35:09 -0700

    Sorted Job methods.

--- UrbanParksProject/src/model/Job.java
+++ UrbanParksProject/src/model/Job.java
@@ -12,71 +12,29 @@ import java.util.List;
  */
 public class Job implements Serializable {
 
+	//Class Constants
     private static final long serialVersionUID = 2L;
-
     public static final int MAX_NUM_JOBS = 10000;
     
-    /**
-     * This value is the identification number of a job.
-     */
+    //Class Variables
     private int myJobID;
-
-    /**
-     * This is the list which holds the e-mail addresses of all volunteers that
-     * have signed up to participate in this job.
-     */
     private List<List<String>> myVolunteerList;
-
     
-    /**
-     * This is the start date of the job.
-     */
     private GregorianCalendar myStartDate;
-
-    /**
-     * This is the ending date of the job.
-     */
     private GregorianCalendar myEndDate;
-
-    /**
-     * This value holds the maximum number of people who can sign up for the light portion
-     * of this job.
-     */
+    
     private int myLightMax;
-
-    /**
-     * This value holds the maximum number of people who can sign up for the
-     * medium portion of this job.
-     */
     private int myMediumMax;
-
-    /**
-     * This value holds the maximum number of people who can sign up for the heavy portion
-     * of this job.
-     */
     private int myHeavyMax;
-
-    /**
-     * This value holds the park that the job will be located at.
-     */
+    
     private String myPark;
-
-    /**
-     * The email address of the Park Manager in charge of the job.
-     */
     private String myManager;
+    
+    private boolean isPast;
 
     /**
-     * This field is used by volunteer class to determine if a job's start date
-     * is in the past (compared to the current time).
-     * Note: this is true if the job is in the past.
-     */
-    private boolean myPast;
-
-    /**
-     * Constructor for job, taking several arguments of integers, Calendar
-     * Strings, and e-mails.
-     * 
+     * Constructor for Job, taking several arguments of integers, Calendar
+     * Strings, and e-mails. 
      * @author Taylor Gorman
      */
     public Job(int theJobID, String thePark, int theLightMax, int theMediumMax,
@@ -96,44 +54,75 @@ public class Job implements Serializable {
         this.myStartDate = stringToCalendar(theStartDate);
         this.myEndDate = stringToCalendar(theEndDate);
         
-        this.myPast = false;
+        this.isPast = false;
     }
 
-    /**
-     * Convert a date string to a Gregorian Calendar object.
-     * 
-     * @param stringDate
-     *            A string representing a date, of format mmddyyyy
-     * @return A Gregorian Calendar object representing that date.
-     * 
-     * @author Taylor Gorman
-     * @version 9 May 2015
-     */
-    private GregorianCalendar stringToCalendar(String stringDate) {
-        int myDay = Integer.parseInt(stringDate.substring(0, 2));
-        int myMonth = Integer.parseInt(stringDate.substring(2, 4));
-        int myYear = Integer.parseInt(stringDate.substring(4, 8));
 
-        return new GregorianCalendar(myYear, myDay, myMonth);
-    }
+    
+    /*=========*
+     * Getters *
+     *=========*/
 
     /**
-     * This is used to return the starting date of the job.
-     * 
-     * @return myStartDate
+     * Return the starting date of the Job as a GregorianCalendar.
      */
     public GregorianCalendar getStartDate() {
         return myStartDate;
     }
 
     /**
-     * This is used to return the ending date of the job.
-     * 
-     * @return myEndDate
+     * Return the ending date of the Job as a Greogriant Calendar.
      */
     public GregorianCalendar getEndDate() {
         return myEndDate;
     }
+    
+    public int getJobID() {
+        return myJobID;
+    }
+
+    public List<List<String>> getVolunteerList() {
+        return myVolunteerList;
+    }
+
+    public int getLightMax() {
+        return myLightMax;
+    }
+
+    public int getLightCurrent() {
+        return getNumberOfSlots(0);
+    }
+
+    public int getMediumMax() {
+        return myMediumMax;
+    }
+
+    public int getMediumCurrent() {
+        return getNumberOfSlots(1);
+    }
+
+    public int getHeavyMax() {
+        return myHeavyMax;
+    }
+
+    public int getHeavyCurrent() {
+        return getNumberOfSlots(2);
+    }
+
+    public String getPark() {
+        return myPark;
+    }
+
+    public String getManager() {
+        return myManager;
+    }
+    
+    
+    
+    
+    /*=============*
+     * Check Slots *
+     *=============*/
 
     /**
      * This method is called when someone needs to know if there are any more
@@ -174,6 +163,43 @@ public class Job implements Serializable {
     public boolean hasRoom() {
         return hasLightRoom() || hasMediumRoom() || hasHeavyRoom();
     }
+    
+    
+    
+    
+    /*============*
+     * Check Date *
+     *============*/
+    
+    public boolean isInPast() {
+        return isPast;
+    }
+
+    public void setInPast(boolean theIsPastFlag) {
+        this.isPast = theIsPastFlag;
+    }
+    
+    
+    
+    /*===============*
+     * Add Volunteer *
+     *===============*/
+    
+    public void addVolunteer(List<String> theVolunteer) {
+        myVolunteerList.add(theVolunteer);
+    }
+    
+    
+    
+    
+    /*================*
+     * Helper Classes *
+     *================*/
+    
+    @Override
+    public String toString() {
+        return myPark.toString();
+    }
 
     /**
      * Return the number of available job slots for a given work grade.
@@ -213,60 +239,21 @@ public class Job implements Serializable {
         }
     }
 
-    public int getJobID() {
-        return myJobID;
-    }
+    
+    /**
+     * Convert a date string to a Gregorian Calendar object. 
+     * 
+     * @param stringDate
+     *            A string representing a date, of format mmddyyyy
+     * @return A Gregorian Calendar object representing that date.
+     * 
+     * @author Taylor Gorman
+     */
+    private GregorianCalendar stringToCalendar(String stringDate) {
+        int myDay = Integer.parseInt(stringDate.substring(0, 2));
+        int myMonth = Integer.parseInt(stringDate.substring(2, 4));
+        int myYear = Integer.parseInt(stringDate.substring(4, 8));
 
-    public List<List<String>> getVolunteerList() {
-        return myVolunteerList;
-    }
-
-    public int getLightMax() {
-        return myLightMax;
-    }
-
-    public int getLightCurrent() {
-        return getNumberOfSlots(0);
-    }
-
-    public int getMediumMax() {
-        return myMediumMax;
-    }
-
-    public int getMediumCurrent() {
-        return getNumberOfSlots(1);
-    }
-
-    public int getHeavyMax() {
-        return myHeavyMax;
-    }
-
-    public int getHeavyCurrent() {
-        return getNumberOfSlots(2);
-    }
-
-    public String getPark() {
-        return myPark;
-    }
-
-    public String getManager() {
-        return myManager;
-    }
-
-    public void addVolunteer(List<String> theV) {
-        myVolunteerList.add(theV);
-    }
-
-    @Override
-    public String toString() {
-        return myPark.toString();
-    }
-
-    public boolean isInPast() {
-        return myPast;
-    }
-
-    public void setIfPast(boolean myPast) {
-        this.myPast = myPast;
+        return new GregorianCalendar(myYear, myDay, myMonth);
     }
 }
