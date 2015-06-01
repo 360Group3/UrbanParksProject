f573e0270f342394a67f593ce2a0ea9fbce56723
Reid Thompson <reidobandedo@gmail.com>
2015-06-01 04:53:37 -0700

    Finalized US files.

--- UrbanParksProject/src/startup/Runner.java
+++ UrbanParksProject/src/startup/Runner.java
@@ -24,8 +24,8 @@ public class Runner {
         LoginUI UI = new LoginUI();
 
         //Set File Names
-        String jobFile = "jobList1.ser";
-        String userFile = "userList1.ser";
+        String jobFile = "rsc/jobList1.ser";
+        String userFile = "rsc/userList1.ser";
         
         /*
          * This constitutes the main, outer loop of the program.
