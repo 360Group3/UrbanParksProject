2ff82a248036657700a7eccd6a770333dab17269
Taylor <tjsg1992@gmail.com>
2015-05-31 21:30:03 -0700

    User Housekeeping.

--- UrbanParksProject/src/model/User.java
+++ UrbanParksProject/src/model/User.java
@@ -13,54 +13,94 @@ import java.util.Objects;
  */
 public abstract class User implements Serializable, Comparable<User> {
 
+	//Class Constant
     private static final long serialVersionUID = 6L;
 
+    //Class Variables
     private String myFirstName;
     private String myLastName;
     private String myEmail;
 
+    /**
+     * General Constructor for a User.
+     * @param theFirstName the first name of the user.
+     * @param theLastName the last name of the user.
+     * @param theEmail the email address of the user.
+     */
     public User(String theFirstName, String theLastName, String theEmail) {
         myFirstName = theFirstName;
         myLastName = theLastName;
         myEmail = theEmail;
     }
+    
+    
+    /*=========*
+     * Getters *
+     *=========*/
 
+    /**
+     * Return the first name of the User.
+     */
     public String getFirstName() {
         return myFirstName;
     }
 
+    /**
+     * Return the last name of the User.
+     */
     public String getLastName() {
         return myLastName;
     }
 
+    /**
+     * Return the email address of the User.
+     */
     public String getEmail() {
         return myEmail;
     }
     
+    
+    
+    
+    /*===========*
+     * Overrides *
+     *===========*/
+    
     @Override
+    /**
+     * Compare users by their last name primarily, and first name secondarily.
+     */
     public int compareTo(User theOther) {
             //Sort by last name
             int result = myLastName.compareTo(theOther.getLastName());
+            
             //If the last names match, then sort by first name
             if (result == 0) {
                 result = myLastName.compareTo(theOther.getFirstName());
             }
+            
             return result;
     }
     
     @Override
-    public boolean equals(Object o){
-        if (o == null)
-            return false;
+    /**
+     * Return true if the two Users share the same email address; false otherwise.
+     */
+    public boolean equals(Object o){    	
+    	boolean isEqual = false;
+        
         if (o instanceof User) {
             User other = (User) o;
-            return myEmail.equals(other.getEmail());
+            isEqual = myEmail.equals(other.getEmail());
         }
         
-        return false;
+        return isEqual;
     }
 
     @Override
+    /**
+     * Hash User according to their first name, last name, and email address.
+     */
     public int hashCode() {
         return Objects.hash(myFirstName, myLastName, myEmail);
     }
